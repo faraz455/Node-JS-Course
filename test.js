e2050f2ffd5e5078f97fb1392b808b39c70f388e
Faraz Khan <you@example.com>
2020-10-21 00:13:29 +0500

    Stream and Buffer

--- test.js
+++ test.js
@@ -41,3 +41,26 @@ else{
     })
 }
 
+
+//5. reading a big file chunk by chunk 
+//6. writing in anther file chunk by chunk
+
+const readStream = fs.createReadStream('./Blogs/blog1.txt');
+const writeStream = fs.createWriteStream('./Blogs/blog2.txt');
+
+
+readStream.on('data', (chunk)=>{
+    //reading data from blod1 chunk by chunk
+    console.log("***********************new chunk starts here***********************");
+    console.log(chunk.toString());
+    
+    //writing data from blog1 to blog2 chunk by chunk*/
+      writeStream.write('/n*********New Chunk**************/n');
+      writeStream.write(chunk.toString());
+})
+
+// 6. writing one file to anther chunk by chunk by single line using pipe 
+const readStream = fs.createReadStream('./Blogs/blog1.txt');
+const writeStream = fs.createWriteStream('./Blogs/blog2.txt');
+readStream.pipe(writeStream);
+
